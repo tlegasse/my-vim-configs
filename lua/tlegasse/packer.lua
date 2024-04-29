87162ddce04a5d57aa0da3d72d4568b54ae3e017
tlegasse <tanner.legasse@gmail.com>
2024-04-29 11:11:45 -0700

    Adding in swp keymap removal

--- lua/tlegasse/packer.lua
+++ lua/tlegasse/packer.lua
@@ -42,8 +42,7 @@ return require('packer').startup(function(use)
             }
         end
     })
-    use 'nvim-tree/nvim-web-devicons'
-    use 'akinsho/bufferline.nvim'
+    use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
 
     -- Code interaction
     use 'phelipetls/jsonpath.nvim'
