825e91878440daf03f8f55f56193b5211fe742d8
tlegasse <tanner.legasse@gmail.com>
2024-04-29 11:25:32 -0700

    Moving keycommand definition

---
+++ after/plugin/misc.lua
@@ -0,0 +1,1 @@
+vim.api.nvim_del_keymap('n', '<leader>swp')
--- lua/tlegasse/remap.lua
+++ lua/tlegasse/remap.lua
@@ -40,5 +40,3 @@ end
 
 -- Map <leader>sw to the toggle_wrap function
 vim.api.nvim_set_keymap('n', '<leader>sw', '<cmd>lua toggle_wrap()<CR>', { noremap = true, silent = true })
-
-vim.api.nvim_del_keymap('n', '<leader>swp')
